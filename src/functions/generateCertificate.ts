76af3138ed910b19bcb6d9ae25f05756c15ee9a8
Ivan Seibel <me@ivanseibel.dev>
2022-11-27 20:56:52 +0000

    Generates certificate using template

--- src/functions/generateCertificate.ts
+++ src/functions/generateCertificate.ts
@@ -1,10 +1,34 @@
 import { APIGatewayProxyHandler } from "aws-lambda";
 import { dbClient } from "../db/dynamodbClient";
+import * as handlebars from "handlebars";
+import * as path from "path";
+import { readFileSync } from "fs";
 
 interface IPayload {
   id: string;
   name: string;
   grade: string
+}
+
+interface ITemplate  extends IPayload {
+  date: string;
+  medal: string;
+}
+
+
+const compileTemplate = (data: ITemplate) => {
+  const { name, grade, date, medal } = data;
+
+  const filePath = path.join(process.cwd(), "src", "templates", "certificate.hbs");
+
+  const html = readFileSync(filePath, "utf-8");
+
+  return handlebars.compile(html)({
+    name,
+    grade,
+    date,
+    medal
+  });
 }
 
 export const handler: APIGatewayProxyHandler = async (event: any = {}): Promise<any> => {
@@ -28,6 +52,24 @@ export const handler: APIGatewayProxyHandler = async (event: any = {}): Promise<
         ':id': id
       }
     }).promise();
+
+    // Format date to yyyy/mm/dd European format
+    const formattedDate = new Date(certificate.Items[0].createdAt).toLocaleDateString('en-GB', {
+      day: 'numeric',
+      month: 'long',
+      year: 'numeric'
+    });
+
+    const medalPath = path.join(process.cwd(), "src", "templates", "medal.png");
+    const medal = readFileSync(medalPath, "base64");
+
+    const compiledCertificate = compileTemplate({
+      id,
+      name,
+      grade,
+      date: formattedDate,
+      medal
+    });
 
     const response = certificate.Items.length > 0 ? certificate.Items[0] : {};
 
